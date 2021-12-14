a75a5a263ac2d2f6ccdc35388c889f9359d7c169
chenzhenxxx <1922816632@qq.com>
2021-12-14 22:34:24 +0800

    czx

--- C_primer_plus/12.8.c
+++ C_primer_plus/12.8.c
@@ -1,41 +1,89 @@
-int * make_array(int elem, int val); 
-void show_array(const int ar [], int n); 
-int main(void) 
-{ 
-    int * pa; 
-    int size; 
-    int value; 
-    printf("Enter the number of elements: "); 
-    while (scanf("%d", &size) == 1 && size > 0) 
-    { 
-        printf("Enter the initialization value: ");
-        scanf("%d", &value); 
-        pa = make_array(size, value); 
-        if (pa) 
-        { 
-            show_array(pa, size); 
-            free(pa); 
-        } 
-        printf("Enter the number of elements (<1 to quit): "); 
-    } 
-    printf("Done.\n"); 
+#include <stdio.h>
+#include <string.h>
+ 
+int x[100]={0},y[100]={0},z[100]={0};
+int main()
+{
+    char a[100],b[100];//定义两个字符数组来接收两个大数；
+    int i,j,m,n,q;
+    scanf("%s %s",a,b);
+    m=strlen(a);
+    n=strlen(b);
+    j=0;
+    for(i=m-1;i>=0;i--)//字符数组转化为倒序int型数组；
+    {
+        x[j]=a[i]-'0';
+        j++;
+    }
+    j=0;
+    for(i=n-1;i>=0;i--)
+    {
+        y[j]=b[i]-'0';
+        j++;
+    }
+    if(m>n)//判断减数与被减数的位数
+    {
+        sub(x,y,m);
+    }
+    else if(m<n)
+    {
+        printf("-");
+        sub(y,x,n);
+    }
+    else
+    {
+        for(i=m-1;i>=0;i--)
+        {
+            if(x[i]!=y[i])
+            {
+                if(x[i]>y[i])
+                {
+                    sub(x,y,m);
+                    break;
+                }
+                else
+                {
+                    printf("-");
+                    sub(y,x,m);
+                    break;
+                }
+            }
+            else
+            {
+                printf("0");
+                printf("\n");
+                break;
+            }
+        }
+    }
     return 0;
 }
-#include <stdlib.h>
-int *make_array(int elem,int val)
+void sub(int x[],int y[],int p)
 {
-    int *p,i;
-    p=(int*)malloc(elem*sizeof(int));
-    for(i=0;i<elem;i++)
-    p[i]=val;
-    return p;
-}
-void show_array(const int*p,int n)
-{    int i;
-    for(i=0;i<n;i++)
-    { 
-        printf("%d ",p[i]);
-        if((i+1)%8==0)
-        printf("\n");
+    int i,o;
+    for(i=0;i<p;i++)//进行相减
+    {
+        if(x[i]>=y[i])
+        {
+            z[i]=x[i]-y[i];
+        }
+        else
+        {
+            z[i]=x[i]-y[i]+10;
+            x[i+1]--;
+        }
     }
+    for(i=p-1;i>=0;i--)//排除前几位为零的情况
+    {
+        if(z[i]!=0)
+        {
+            o=i;
+            break;
+        }
+    }
+    for(i=o;i>=0;i--)//倒序打出结果
+    {
+        printf("%d",z[i]);
+    }
+    printf("\n");
 }
