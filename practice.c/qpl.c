3490e38c23d2aea8ff70e39cbc9071b88fa41b2f
chenzhenxxx <1922816632@qq.com>
2022-01-01 13:00:22 +0800

    czx

--- practice.c/qpl.c
+++ practice.c/qpl.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
-#include <stdio.h>
-int a[10],book[10],n;
-void dfs(int step)
-{  int i;
-    if(step==n+1)
+int total, n,m, zdx, zdy, book[50][50], a[50][50];
+void dfs(int x, int y)
+{
+    int tx, ty, i;
+    int z[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
+    if (x == zdx && y == zdy)
     {
-        for(i=1;i<=n;i++)
-        printf("%d",a[i]);
-        printf("\n");
+        total++;
         return;
     }
-
-    for(i=1;i<=n;i++)
+    for (i = 0; i <= 3; i++)
     {
-        if(book[i]==0)
+        tx =x+z[i][0];
+        ty =y+ z[i][1];
+        if (tx < 1 || tx > zdx || ty < 1 || ty > zdy)
+            continue;
+        if (a[tx][ty] == 0 && book[tx][ty] == 0)
         {
-            a[step]=i;
-            book[i]=1;
-            dfs(step+1); 
-            book[i]=0;
+            book[tx][ty] = 1;
+            dfs(tx, ty);
+            book[tx][ty] = 0;
         }
     }
     return;
 }
 main()
 {
-    
-    scanf("%d",&n);
-    dfs(1);
-    return 0;
+    int k, ge,x,y;
+    scanf("%d %d %d", &n, &m, &ge);
+    scanf("%d %d %d %d", &x, &y, &zdx, &zdy);
+    int zx, zy;
+    for (k = 1; k <= ge; k++)
+    {
+        scanf("%d %d", &zx, &zy);
+        a[zx][zy] = 1;
+    }
+    a[x][y]=1;
+    dfs(x,y);
+    printf("%d",total);
+   
 }
