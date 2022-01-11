e333247bdead8be9e507b30808b09581f4a82da3
chenzhenxxx <1922816632@qq.com>
2022-01-11 10:32:09 +0800

    czx

--- test.c
+++ test.c
@@ -1,9 +1,30 @@
-#include <stdio.h>
-#include <string.h>
+ssize_t strbuf_read_file(struct strbuf *sb, const char *path, size_t hint)
+{
+    FILE*fp;
+    char *p=(char*)malloc(BUFSIZ);
+    if((fp=fopen(path,"r"))==NULL)
+    {   printf("can't open the file\n");
+        exit(1);
+    }
+    sb->alloc+=hint ? hint : 8192;
+    if(hint!=0)
+    realloc(sb->buf,sb->alloc*(sizeof(char)));
+    size_t len=0;
+     //while(fgets(txt,255,fp)!=NULL)
+     		//strcat(sb->buf,txt);
+    //sb->len=strlen(sb->buf);
+    int sign=0;
+      while(sign=getline(&p,&len,fp)!=NULL)
+      {
+           strcat(sb->buf,p);
+        sb->len+=strlen(p);
+      }
+    if(fclose(fp)!=0)
+    {
+        printf("can't close the file.\n");
+        exit(1);
+    }
+    free(p);
+   return sb->len;
 
-int main() {
-  char str[6] = "xiyou";
-  char str1[10]="linux";
-  strcat(str,str1);
-  printf("%s\n", str);
 }
