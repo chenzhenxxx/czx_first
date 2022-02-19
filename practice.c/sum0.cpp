9a5161a23b6c13e7ebdb63e484da43cc4fdf8dc0
chenzhenxxx <1922816632@qq.com>
2022-02-19 21:29:41 +0800

    czx

---
+++ practice.c/sum0.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int a[5000][5];
+int main()
+{
+   int n,i,j;
+   cin>>n;
+   for(i=0;i<n;i++)
+   for(j=0;j<4;j++)
+     cin>>a[i][j];
+     
+}
