67f8c9818207013a5d092e6cdd79c266e194a2f4
chenzhenxxx <1922816632@qq.com>
2022-02-24 22:23:04 +0800

    czx

---
+++ practice.c/zhuangxiang.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+bool cmp(int a,int b)
+{
+    return a>b;
+}
+int main()
+{
+    int r,n,cnt=0;
+    int a[10000];
+    int dp[1000];
+    cin>>r;
+    cin>>n;
+    int i;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++)
+    for(int j=r;j>=a[i];j--)
+     dp[j]=max(dp[j],dp[j-a[i]]+a[i]);
+    printf("%d",r-dp[r]);
+
+}
