f9391246f103eb8557e1349eac21f768cd47ef8c
chenzhenxxx <1922816632@qq.com>
2022-03-13 14:25:16 +0800

    CZX

--- test.c
+++ test.c
@@ -1,80 +1,59 @@
-#include <stdio.h>
-main()
-{   int terminator; 
-      int i;
-      char p[10][30]={0};
-    int count=0;
-    char str[]="\0ZZ\0\0  123 345  Z  3123  Z 3123  ";
-    int len=33;
-    char ch='Z';
-    int flag=0;
-    char result[100];
-    int k=0;
-    for(i=0;i<len;i++)
+#include<stdio.h>
+#include <linux/limits.h> //包含PATH_MAX =260
+#include<string.h>
+#include<sys/stat.h> //提供 stat函数
+#include<sys/types.h>
+#include<stdlib.h>
+#include<pwd.h>  //找uid
+#include<grp.h>  //找gid
+#include<time.h>  //转换时间
+#include<unistd.h>
+#include<dirent.h>
+void name_quicksort(char filename[256][PATH_MAX+1],int begin,int end)
+{   if(begin>=end)
+    return;
+    int i=begin;
+    int j=end;
+    char tmpname[100];
+    strcpy(tmpname,filename[begin]);
+    tmpname[strlen(filename[begin])]='\0';
+    while(i!=j)
     {
-        if(flag==0&&str[i]==ch)
-        continue;
-        else if(flag==0&&str[i]!=ch)
+        while((i<j)&&(strcmp(filename[j],tmpname)>0))
+        j--;
+        while((i<j)&&(strcmp(filename[i],tmpname)<0))
+        i++;
+        if(i<j)
         {
-            result[k]=str[i];
-            k++;
-            flag=1;
+            char tmp[100];
+            strcpy(tmp,filename[i]);
+            tmp[strlen(filename[strlen(filename[i])])]='\0';
+            strcpy(filename[i],filename[j]);
+            strcpy(filename[j],tmp);
+
         }
-        else if(flag==1&&str[i]==ch)
-        {
-           for(int m=0;m<k;m++)
-           p[count][m]=result[m];
-           for(int j=0;j<100;j++)
-           result[j]='\0';
-            flag=0;
-            count++;
-            k=0;
-        }
-        else if(flag==1&&str[i]!=ch)
-        result[k++]=str[i];
+
     }
-     for(i=0;i<count;i++)
-     {
-         for(int j=0;j<20;j++)
-     {
-         printf("%c",p[i][j]);
-     }
-     printf("\n");
-     }
+         strcpy(tmpname,filename[i]);
+         strcpy(filename[i],filename[begin]);
+         strcpy(filename[begin],tmpname);
+         name_quicksort(filename,begin,i-1);
+         name_quicksort(filename,i+1,end);
+         
 
 }
-    int count=0;
-      int i;
-     struct strbuf **p;
-      p=(struct strbuf**)malloc(sizeof(struct strbuf*));
-      char q[2]; 
-      q[0]=(char)terminator;
-      q[1]='\0';
-       char s[len+1];
-       for(i=0;i<=len;i++)
-       s[i]=str[i];
-       //strcpy(s,str);
-       struct strbuf *k;
-       for(i=0;i<len;i++)
-       {
-           if(s[i]=='\0')
-           s[i]=(char)terminator;
+int main()
+{
+    char a[100][20];
+    for(int i=0;i<5;i++)
+    {
+        scanf("%s",a[i]);
+    }
 
-       }
-      char*r=strtok(s,q);//分割字符串函数。
-      while(r!=NULL&&count<max)
-      {
-
-          k=(struct strbuf*)malloc(sizeof(struct strbuf));
-         {
-          strbuf_init(k,strlen(r)+1); //'\0'
-          strbuf_addstr(k,r);
-         }
-          p[count]=k;
-           count++;
-          p=(struct strbuf**)realloc(p,sizeof(struct strbuf*)*(count+1));
-          r=strtok(NULL,q);
-
-      }
-      p[count]= NULL;
-      return p;
+    name_quicksort(a,0,4);
+     
+     for(int i=0;i<5;i++)
+    {
+        printf("%s\n",a[i]);
+    }
+}
--- testt.c
+++ testt.c
@@ -1,9 +1,37 @@
+#include "myls2.h"
 
-#include <stdio.h>
-#include <time.h>
- 
-int main ()
+
+int main(const char argc, const char** argv)
 {
-  printf("%-10d\n",101010);
-  printf("%10d",101010);
+    char path[PATH_MAX+1] = {};
+
+    if(argc == 2 && !(strcmp(argv[1],"-R"))) // 判断命令格式
+        strcpy(path,".");
+    else if(argc != 3)
+    {
+        printf("格式有误！ \n");
+        exit(EXIT_FAILURE);
+    }
+    else
+        strcpy(path,argv[2]);
+
+    if(!(strcmp(argv[1],"-R")))
+    {
+        struct stat file_message = {};
+        int ret_stat = lstat(path, &file_message);
+
+        if(ret_stat == -1)
+               error_printf("stat");
+
+        if(S_ISDIR(file_message.st_mode)) // 判断是否为目录
+            list_dir(path);
+        else
+        printf("It is not dir!");
+    }
+    else
+    {
+        printf("error in main!\n");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
 }
