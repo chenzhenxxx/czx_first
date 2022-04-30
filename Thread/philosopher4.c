9d1c9d3e7e722e9bc17c412bf82e8d77e8c10abd
chenzhenxxx <1922816632@qq.com>
2022-04-30 21:20:40 +0800

    czx

--- Thread/philosopher4.c
+++ Thread/philosopher4.c
@@ -1,9 +1,9 @@
-//一次最多允许四个哲学家竞争
+//奇数编号哲学家先拿左筷子，再拿右筷子
+//偶数编号哲学家先拿右筷子，再拿左筷子
 #include<stdio.h>
 #include<pthread.h>
 # include <semaphore.h>
 sem_t pthread_kz[5];
-sem_t count;
 pthread_mutex_t mutex;
 void * zxj(void *arg)
 {
@@ -21,28 +21,34 @@ void * zxj(void *arg)
     }
       while(1)
       {   sleep(1); //思考
-           sem_wait(&count);
            
+           if(!(i&2))
+           {
           sem_wait(&pthread_kz[left]);
           printf("philosopher %d fetches chopstick %d\n",i,left);
           sem_wait(&pthread_kz[right]);
            printf("philosopher %d fetches chopstick %d\n",i,right);
            printf("philosopher %d is eating\n",i);
-
+           }
+           else
+           {
+               sem_wait(&pthread_kz[right]);
+          printf("philosopher %d fetches chopstick %d\n",i,right);
+          sem_wait(&pthread_kz[left]);
+           printf("philosopher %d fetches chopstick %d\n",i,left);
+           printf("philosopher %d is eating\n",i);
+           }
            sleep(2);
-        
            sem_post(&pthread_kz[right]);
             printf("philosopher %d release chopstick %d\n",i,right);
            sem_post(&pthread_kz[left]); 
            printf("philosopher %d release chopstick %d\n",i,left);
-           sem_post(&count);
       }
 }
 int main()
 {
     pthread_t pid[5];
     pthread_mutex_init(&mutex,NULL);
-    sem_init(&count,0,4);
     for(int i=0;i<5;i++)
     {
         sem_init(&pthread_kz[i],0,1);
