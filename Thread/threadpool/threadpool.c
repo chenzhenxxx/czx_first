b04c7d1d899e52b9553246210bda13cb49bdb17e
chenzhenxxx <1922816632@qq.com>
2022-05-05 16:54:42 +0800

    czx

--- Thread/threadpool/threadpool.c
+++ Thread/threadpool/threadpool.c
@@ -1,5 +1,6 @@
 #include "threadpool.h"
 #include<pthread.h>
+#define NUM 2
 typedef struct Task
 {
     void (*function)(void * arg);
@@ -69,14 +70,14 @@ ThreadPool *threadPoolCreate(int min,int max,int Capacity)
       if(pthread_mutex_init(&pool->mutexpool,NULL)!=0||
       pthread_mutex_init(&pool->mutexbusy,NULL)!=0||
       pthread_cond_init(&pool->notempty,NULL)!=0||
-      pthread_cond_init(&pool->notfull,NULL)!=0)
+      pthread_cond_init(&pool->notfull,NULL)!=0) //初始化互斥锁和条件变量
       {
           printf("init error\n");
       }
       //创建线程
-      pthread_create(&pool->managerID,NULL,manager,NULL);
+      pthread_create(&pool->managerID,NULL,manager,pool);
       for(int i=0;i<min;i++)
-      pthread_create(&pool->threadID[i],NULL,worker,NULL);
+      pthread_create(&pool->threadID[i],NULL,worker,pool);
       
       return pool;
     }while(0);
@@ -99,33 +100,158 @@ void* worker(void* arg)
     while(1)
     {
         pthread_mutex_lock(&pool->mutexpool);
-    while(pool->queuesize==0&&!pool->shutdown)  //当前队列为空
+    while(pool->queuesize==0&&!pool->shutdown)  //当前队列为空但是线程池没有关闭
      {     //阻塞
           pthread_cond_wait(&pool->notempty,&pool->mutexpool);
+          if(pool->killNum!=0)
+           {   pool->killNum--;
+               if(pool->liveNum>pool->minNum)
+               {   pool->liveNum--;
+                   pthread_mutex_unlock(&pool->mutexpool);
+                   threadExit(pool);
+               }
+           }
      }
      if(pool->shutdown)
       {
           pthread_mutex_unlock(&pool->mutexpool);
-          pthread_exit(NULL);
+          threadExit(pool);
       }
+      //取出任务
      Task task;
     task.function=pool-> Q[pool->queuehead].function;
     task.arg=pool-> Q[pool->queuehead].arg;
     pool->queuehead=(pool->queuehead+1)%pool->queuecapacity;   
     pool->queuesize--;
+     
+    pthread_cond_singal(&pool->notfull);  //唤醒生产者
+    pthread_mutex_unlock(&pool->mutexpool);
+    printf("pthread %ld start doing work\n",pthread_myself());
     pthread_mutex_lock(&pool->mutexbusy);
     pool->busyNum++;
      pthread_mutex_unlock(&pool->mutexbusy);
      task.function(task.arg);
+     free(task.arg);
+     task.arg=NULL;
      printf("thread %ld end working..\n");
      pthread_mutex_lock(&pool->mutexbusy);
     pool->busyNum--;
      pthread_mutex_unlock(&pool->mutexbusy);
 
+
     
 
 
 
-     pthread_mutex_unlock(&pool->mutexpool);
+     
+    }
+}
+void* manager(void* arg)
+{
+    ThreadPool * pool=(ThreadPool*)arg;
+    while(!pool->shutdown)
+     {
+         sleep(3);
+         pthread_mutex_lock(&pool->mutexpool);
+          int queueNum=pool->queuesize; //获取任务个数
+          int  liveNum=pool->liveNum;  //获取存活个数
+        pthread_mutex_unlock(&pool->mutexpool);
+
+        pthread_mutex_lock(&pool->mutexbusy);
+          int busyNum=pool->busyNum;
+        pthread_mutex_unlock(&pool->mutexbusy);
+
+        //创建线程
+         if(queueNum>liveNum&&liveNum<pool->maxNum)
+          {
+             pthread_mutex_lock(&pool->mutexpool);
+             int cnt=0;
+              for(int i=0;i<pool->maxNum&&cnt<NUM&&pool->liveNum<pool->maxNum;i++)
+               {
+                   if(pool->threadID[i]==0) //说明该线程没使用
+                    {   pthread_create(&pool->threadID[i],NULL,worker,pool);
+                        pool->liveNum++;
+                        cnt++;
+                    }
+
+               }
+             pthread_mutex_unlock(&pool->mutexpool);
+
+          }
+
+         //销毁线程
+
+          if(liveNum>2*busyNum&&liveNum>pool->minNum)
+           {
+               pthread_mutex_lock(&pool->mutexpool);
+                pool->killNum=NUM;
+                pthread_mutex_unlock(&pool->mutexpool);
+                for(int i=0;i<NUM;i++)
+                 {
+                     pthread_cond_signal(&pool->notempty);
+                 }
+
+           }
+     }
+     return NULL;
+}
+void threadExit(ThreadPool* pool)
+{
+    pthread_t tid=pthread_myself();
+    for(int i=0;i<pool->maxNum;i++)
+     {
+         if(pool->threadID[i]==tid)
+          {
+              pool->threadID[i]=0;
+              printf("threadid exit called,%ld is exiting ..\n",tid);
+          }
+     }
+     pthread_exit(NULL);
+}
+void threadPoolAdd(ThreadPool* pool, void(*func)(void*), void* arg)
+{
+    if(pool->queuesize==pool->queuecapacity&&!pool->shutdown) //任务队列满了但是线程池没有关闭
+    {
+        pthread_cond_wait(&pool->notfull,&pool->mutexpool);
+
+    }
+
+    if(pool->shutdown)
+     {
+         pthread_mutex_unlock(&pool->mutexpool);
+         return;
+     }
+     pool->Q[pool->queuerear].function=func;
+     pool->Q[pool->queuerear].arg=arg;
+     pool->queuerear=(pool->queuerear+1)%pool->queuecapacity; //维护环形队列
+     pool->queuesize++;
+     pthread_cond_singal(&pool->notempty); //唤醒消费者
+}
+int threadPoolBusyNum(ThreadPool* pool)
+{
+    pthread_mutex_lock(&pool->mutexbusy);
+    int busynum=pool->busyNum;
+    pthread_mutex_unlock(&pool->mutexbusy);
+    return busynum;
+}
+int threadPoolAliveNum(ThreadPool* pool)
+{
+    pthread_mutex_lock(&pool->mutexpool);
+    int livenum=pool->liveNum;
+    pthread_mutex_unlock(&pool->mutexpool);
+    return livenum;
+}
+int threadPoolDestroy(ThreadPool* pool)
+{
+    if(pool==NULL)
+    {
+        return -1;
+    }
+    pool->shutdown=1; //关闭线程池
+
+    pthread_join(&pool->managerID,NULL);  //回收管理者
+    for(int i=0;i<pool->maxNum;i++)
+    {
+        pthread_
     }
 }
--- Thread/生产者消费者/producer-consumer.c
+++ Thread/生产者消费者/producer-consumer.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<string.h>
 #include<pthread.h>
-pthread_cond_t cond;
+#include<stdlib.h>
+pthread_cond_t notFull;
+pthread_cond_t notEmpty;
 pthread_mutex_t mutex;
 int THREAD_MAX;
 // Single-producer , single-consumer Queue
@@ -21,7 +23,8 @@ void SPSCQueueDestory(SPSCQueue *pool);
 void SPSCQueueDestory(SPSCQueue *pool)
 {
     pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond);
+    pthread_cond_destroy(&notEmpty);
+    pthread_cond_destroy(&notFull);
     free(pool->q);
     free(pool);
     
@@ -29,7 +32,8 @@ void SPSCQueueDestory(SPSCQueue *pool)
 SPSCQueue *SPSCQueueInit(int capacity)
 {
     pthread_mutex_init(&mutex,NULL);
-    pthread_cond_init(&cond,NULL);
+    pthread_cond_init(&notEmpty,NULL);
+     pthread_cond_init(&notFull,NULL);
     SPSCQueue * pool=(SPSCQueue *)malloc(sizeof(SPSCQueue));
     (pool->q)=(int *)malloc(sizeof(int)*capacity);
     pool->num=0;
@@ -38,40 +42,42 @@ SPSCQueue *SPSCQueueInit(int capacity)
 
 
 }
-void SPSCQueuePush(SPSCQueue *pool, void *s)
+void SPSCQueuePush(SPSCQueue *pool, void *s) //生产者
 {
    while(1)
-    {
+    {   pthread_mutex_lock(&mutex);
         if(pool->num<THREAD_MAX)
          {
-            pthread_mutex_lock(&mutex);
+            
             pool->q[(pool->num)]=rand()%1000;
             printf("producer--->%d\n",pool->q[pool->num]);
             (pool->num)++;
-            pthread_mutex_unlock(&mutex);
-            pthread_cond_signal(&cond);
+           
+            pthread_cond_signal(&notEmpty);
 
          }
          else
-         {
+         {   
              printf("already full\n");
+             pthread_cond_wait(&notFull,&mutex);
          }
+          pthread_mutex_unlock(&mutex);
          sleep(rand()%2);
     }
 }
-void *SPSCQueuePop(SPSCQueue *pool)
+void *SPSCQueuePop(SPSCQueue *pool)  //消费者
 {
     while(1)
     {
         pthread_mutex_lock(&mutex);
         if(pool->num==0)
          {   
-             pthread_cond_wait(&cond,&mutex);
+             pthread_cond_wait(&notEmpty,&mutex);
          }
          
            (pool->num)--;
            printf("consumer--->%d\n",pool->q[pool->num]);
-            
+           pthread_cond_signal(&notFull);
          pthread_mutex_unlock(&mutex);
          sleep(rand()%3);
     }
