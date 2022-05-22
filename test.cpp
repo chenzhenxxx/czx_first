2d67e34d64bafdb051267766a47beb5c6ee61508
chenzhenxxx <1922816632@qq.com>
2022-05-22 21:04:41 +0800

    czx

--- test.cpp
+++ test.cpp
@@ -1,37 +1,29 @@
 #include<bits/stdc++.h>
+#define LL long long
+#define PB push_back
+ 
 using namespace std;
-int big[10],small[10],total[10];
+ 
+const int N=1e6+10,mod=1e9+7;
+int n,m;
+int a[N];
+LL s[N];
 int main()
 {
-
-   long long ans=3;
-   big[1]=1;
-   small[1]=0;
-   total[1]=1;
-   int cnt=1;
-   for(int i=2;i<=6;i++)
-    {   cnt++;
-        big[i]=big[i-1];
-        if(cnt>=4)
-        {
-            big[i]+=small[i-3];
-
-            
+    for(int i=1;i<N;i++){
+        for(int j=i;j<N;j+=i){
+            if(1ll*i*i<=j)a[j]=i;
         }
-        total[i]=total[i-1]+big[i];
-        small[i]=big[i-1];
-        ans+=3*total[i];
     }
-    long long  n;
-    while(cin>>n&&n!=0)
-    {
-    if(n>=ans)
-    cout<<"DAAAAAMN!"<<endl;
-    else
-    cout<<"AOLIGEI!"<<endl;
+    for(int i=1;i<N;i++){
+        s[i]=s[i-1]+(a[i]+i/a[i])/2;
+        s[i]%=mod;
     }
-
-   
-
-
+    int t;
+    cin>>t;
+    while(t--){
+        scanf("%d",&n);
+        printf("%lld\n",s[n]);
+    }
+    return 0;
 }
