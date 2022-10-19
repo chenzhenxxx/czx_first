9d670f19bd9c3bafb90d4d31a3ea6645d63545ce
chenzhenxxx <1922816632@qq.com>
2022-10-19 21:28:13 +0800

    py

--- 1.py
+++ 1.py
@@ -1,2 +1,12 @@
-x=input()
-print(x[1])
+n=int(input())
+x={}
+while n>0 :
+    g=input()
+    s=g.split(' ')
+    for i in range (1,int(s[0])+1):
+        x[s[i]]=1
+    print(len(x))
+    tmp=sorted(x.keys())
+    print(*tmp)
+    n=n-1
+    
