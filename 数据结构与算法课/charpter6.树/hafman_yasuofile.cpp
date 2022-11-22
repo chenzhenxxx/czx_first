f64149dbf6c003e05abe9302f2b9b2d354d9ea53
chenzhenxxx <1922816632@qq.com>
2022-11-22 23:09:44 +0800

    class

--- 数据结构与算法课/charpter6.树/hafman_yasuofile.cpp
+++ 数据结构与算法课/charpter6.树/hafman_yasuofile.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include<bits/stdc++.h>
+using namespace std;
+map<char,string> password;
+
 typedef struct ht
 {
     int weight;
@@ -9,18 +13,18 @@ typedef struct ht
     struct ht *lchild;
     struct ht *rchild;
 } HT;
-HT *InitHT()  //初始化
+HT *InitHT() //初始化
 {
     HT *huffman = (HT *)malloc(sizeof(HT));
     huffman->next = NULL;
     huffman->lchild = huffman->rchild = NULL;
     return huffman;
 }
-void InsertHuffman(HT *huffman, HT *node)  //插入元素
+void InsertHuffman(HT *huffman, HT *node) //插入元素
 {
     HT *p = huffman;
     HT *q = p->next;
-    while (q && node->weight > q->weight)   //保证有序
+    while (q && node->weight > q->weight) //保证有序
     {
         p = q;
         q = q->next;
@@ -28,7 +32,7 @@ void InsertHuffman(HT *huffman, HT *node)  //插入元素
     node->next = q;
     p->next = node;
 }
-HT *DelLink(HT *huffman, int index)  //取出第index元素并从链表中删除
+HT *DelLink(HT *huffman, int index) //取出第index元素并从链表中删除
 {
     HT *p, *s;
     int i = 0;
@@ -47,7 +51,7 @@ HT *DelLink(HT *huffman, int index)  //取出第index元素并从链表中删除
     // free(s);
     return s;
 }
-void PrintTree(HT *huffman)   //打印哈夫曼树用（）判别子树
+void PrintTree(HT *huffman) //打印哈夫曼树用（）判别子树
 {
     if (huffman != NULL)
     {
@@ -63,7 +67,7 @@ void PrintTree(HT *huffman)   //打印哈夫曼树用（）判别子树
         }
     }
 }
-HT *CreateHuffmanTree(HT *huffman, int n)  //创建哈夫曼树
+HT *CreateHuffmanTree(HT *huffman, int n) //创建哈夫曼树
 {
     HT *min1, *min2, *p;
     HT *node;
@@ -71,7 +75,7 @@ HT *CreateHuffmanTree(HT *huffman, int n)  //创建哈夫曼树
     p = huffman;
     for (int i = 1; i < n; i++)
     {
-        min1 = DelLink(p, 1);             //取出最小的两个元素进行运算
+        min1 = DelLink(p, 1); //取出最小的两个元素进行运算
         min2 = DelLink(p, 1);
         w_sum = min1->weight + min2->weight;
         node = (HT *)malloc(sizeof(HT));
@@ -83,8 +87,8 @@ HT *CreateHuffmanTree(HT *huffman, int n)  //创建哈夫曼树
     }
     return node;
 }
-void Huffman_code(HT *tree, int len)    //哈夫曼编码
-{
+void Huffman_code(HT *tree, int len) //哈夫曼编码
+{ 
     if (!tree)
     {
         return;
@@ -92,12 +96,13 @@ void Huffman_code(HT *tree, int len)    //哈夫曼编码
     static int a[20];
     int i;
     if (!tree->lchild && !tree->rchild)
-    {
+    {   string s;
         printf("%c的哈夫曼编码为: ", tree->data);
         for (int i = 0; i < len; i++)
-        {
+        {   s+=a[i];
             printf("%d", a[i]);
         }
+        password[tree->data]=s;
         printf("\n");
     }
     else
@@ -115,64 +120,64 @@ int main()
     HT *tree;
     int num, w, i; //长度
     char data;
-    printf("输入元素个数:");
-    scanf("%d", &num);
-    getchar();
-    for (i = 0; i < num; i++)
-    {
-        printf("第%d个元素的字符:", i + 1);
-        scanf("%c", &data);
-        getchar();
-        printf("第%d个元素的权重:", i + 1);
-        scanf("%d", &w);
-        getchar();
-        HT *node = (HT *)malloc(sizeof(HT));
-        ;
-        node->data = data;
-        node->weight = w;
-        InsertHuffman(ht, node);
-    }
-    printf("******************\n");
-    tree = CreateHuffmanTree(ht, num);
-    printf("哈夫曼树:");
-    PrintTree(tree);
-    printf("\n");
-    Huffman_code(tree, 0);
-
     HT *htt = InitHT();
     HT *treee;
-    char buf[512];
-    printf("请输入字符串！\n");
-    scanf("%s", buf);
-    int flag[128];
-    int flag_len=0;
-    memset(flag, 0, sizeof(flag));
-    for (int i = 0; i < strlen(buf); i++)
-    {
-        if (flag[buf[i]] == 0)
-        {
-            flag_len++;
-        }
-        flag[buf[i]]++;
-    }
 
-    for (int i = 0; i < 128; i++)
+    printf("请输入读取文件地址!");
+    char path[128];
+    char buf[512];
+    int flag[128];
+    int flag_len = 0;
+    FILE *fp,*fq;
+    scanf("%s", path);
+    if ((fp=fopen(path, "r"))==NULL)
     {
-        if (flag[i] != 0)
+        printf("open file error！\n");
+    }
+    while ((fread(buf, sizeof(char),512,fp))!=0)
+    {
+        memset(flag, 0, sizeof(flag));
+       
+        for (int i = 0; i < strlen(buf); i++)
         {
-            HT *node = (HT *)malloc(sizeof(HT));
-            node->data = (char)i;
-            node->weight = flag[i];
-            printf("%c %d\n", node->data, node->weight);
-            InsertHuffman(htt, node);
+            if (flag[buf[i]] == 0)
+            {
+                flag_len++;
+            }
+            flag[buf[i]]++;
         }
+
+        for (int i = 0; i < 128; i++)
+        {
+            if (flag[i] != 0)
+            {
+                HT *node = (HT *)malloc(sizeof(HT));
+                node->data = (char)i;
+                node->weight = flag[i];
+                printf("%c %d\n", node->data, node->weight);
+                InsertHuffman(htt, node);
+            }
+        }
+        memset(buf, 0, sizeof(buf));
     }
     printf("******************\n");
-    treee = CreateHuffmanTree(htt,flag_len);
+    treee = CreateHuffmanTree(htt, flag_len);
     printf("哈夫曼树:");
     PrintTree(treee);
     printf("\n");
     Huffman_code(treee, 0);
-
+    fclose(fp);
+    if ((fp=fopen(path, "r"))==NULL)
+    {
+        printf("open file error！\n");
+    }
+    if((fq=fopen("./2.txt","a+"))==0)
+    while ((fread(buf, sizeof(char),512,fp))!=0)
+     {
+         for (i=0;i<strlen(buf);i++)
+          {
+            fwrite(password[buf[i]].c_str(),)
+          }
+     }
     return 0;
 }
