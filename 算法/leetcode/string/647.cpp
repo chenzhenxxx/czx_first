4531810938f4283e173148dceb2fba7c0e8d20c9
chenzhenxxx <1922816632@qq.com>
2023-05-23 21:46:56 +0800

    ‘双指针647'

--- 算法/leetcode/string/647.cpp
+++ 算法/leetcode/string/647.cpp
@@ -1,3 +1,4 @@
+//dp
 class Solution {
 public:
     int countSubstrings(string s) {
@@ -19,5 +20,28 @@ public:
               }
           }
           return result;
+    }
+};
+
+
+//双指针
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int result = 0;
+        for (int i = 0; i < s.size(); i++) {
+            result += extend(s, i, i, s.size()); // 以i为中心
+            result += extend(s, i, i + 1, s.size()); // 以i和i+1为中心
+        }
+        return result;
+    }
+    int extend(const string& s, int i, int j, int n) {
+        int res = 0;
+        while (i >= 0 && j < n && s[i] == s[j]) {
+            i--;
+            j++;
+            res++;
+        }
+        return res;
     }
 };
